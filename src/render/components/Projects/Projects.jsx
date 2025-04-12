c74268f1ebdcf331cd87fce0cf63a63a94258fcd
John Abayan <johnwpabayan@gmail.com>
2025-04-13 01:40:48 +0800

    layout changes

--- src/render/components/Projects/Projects.jsx
+++ src/render/components/Projects/Projects.jsx
@@ -7,8 +7,10 @@ import RoundedSquareOne from './img/rounded-square-1.svg'
 import RoundedSquareOutlineOne from './img/rounded-square-outline-1.svg'
 import TriangleOne from './img/triangle-1.svg'
 import StarTwo from './img/star-2.svg'
-
-import ProjectsIntroElements from './ProjectsIntroElements'
+import RoundedSquareTwo from './img/rounded-square-2.svg'
+import StarThree from './img/star-3.svg'
+import LineLongTwo from "./img/line-long-2.svg"
+import LineShortTwo from "./img/line-short-2.svg" 
 
 
 const introElements = [
@@ -43,19 +45,55 @@ const introElements = [
     {
         src: StarTwo,
         className: "star-two"
+    },
+    {
+        src: RoundedSquareTwo,
+        className: "rounded-square-two"
+    },
+    {
+        src: StarThree,
+        className: "star-three"
+    },
+    {
+        src: LineLongTwo,
+        className: "line-long-two"
+    },
+    {
+        src: LineShortTwo,
+        className: "line-short-two"
     }
 ]
 
 
+const projectElements = [
+
+]
+
+
+const projectsInfo = [
+    {
+        img: "",
+        link:"",
+        title: "",
+        description:""
+    }
+]
+
 function Projects(){
     return(
         <>
-            <section className="projects-intro">
+            <div className="projects-intro">
                 <h1 className="projects-intro-text" data-content="Projects">Projects</h1>
-                <ProjectsIntroElements elements={introElements}/>
-            </section>
+                {
+                    introElements.map( e =>{
+                        return(
+                            <img src={e.src} className={e.className}/>
+                        )
+                    })
+                }
+            </div>
             <div id="projects" className="projects-section">
-                <h1> MY WORKS </h1>
+                
             </div>
         </>
     );
