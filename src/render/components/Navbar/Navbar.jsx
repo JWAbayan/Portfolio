d6c615a1f6d8db5d08d899c9880033e8251b00a4
John Abayan <johnwpabayan@gmail.com>
2025-04-21 15:55:39 +0800

    added navbar layout for mobile

--- src/render/components/Navbar/Navbar.jsx
+++ src/render/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './navbar-styles.css'
 
 import personalLogo from '../../assets/img/personal-logo_1.svg'
 import expandMenuFilled from '../../assets/img/menu-collapse-filled.svg'
 import collapseMenuFilled from '../../assets/img/angle-left-filled.svg'
 import nightModeFilled from '../../assets/img/night-filled_1.svg'
+import { div } from "three/tsl";
+import { Example } from "@react-three/drei";
 
 const navlinks = [
   {
@@ -21,45 +23,72 @@ const navlinks = [
   },
 ]
 
+
+function ToggleableMenu({opened}){
+
+  useEffect(()=>{
+    document.body.style.overflow = opened ? "hidden" : ""   
+
+    return (()=>{document.body.style.overflow = ""})
+  },[]) 
+
+  return(
+    <div className="toggleable-menu">
+      {/* <img className="mobile-personal-logo" src={personalLogo} alt="Personal Logo"/>
+      <div className="menu-links">
+        {
+          navlinks.map(link => {
+            return(
+              <button key={link.id} onClick={link => navigateToSection(link)}>
+                <h3 id={link.id} className="nav-links-text">{link.title}</h3>
+              </button>
+            );
+          })   
+        }
+        <button className="action-button">
+          <img src={expandMenuFilled} alt="Personal Logo"/>
+        </button>
+      </div> */}
+    </div>
+  );
+}
+
 function Navbar(){
 
-    const [menuExpanded, expandMenu] = useState(true);
+    const [menuOpened, openMenu] = useState(false);
 
     function navigateToSection(link){
       
     }
 
     return(
+      <>
         <div className="nav-bar">
-        <img className="personal-logo" src={personalLogo} alt="Personal Logo"/>
-        <div className="nav-actions">
-            { 
-              menuExpanded ?
-                <div className="nav-links">
-                  {
-                    navlinks.map(link => {
-                      return(
-                        <button key={link.id} onClick={link => navigateToSection(link)}>
-                          <h3 id={link.id} className="nav-links-text">{link.title}</h3>
-                        </button>
-                      );
-                    })   
-                  }
-                </div> 
-              : null
-            }
-          <button className="action-button" onClick={()=>{expandMenu(!menuExpanded)}}>
+          <img className="personal-logo" src={personalLogo} alt="Personal Logo"/>
+          <div className="nav-actions">
+          <div className="nav-links">
             {
-              menuExpanded ? 
-              <img src={collapseMenuFilled} alt="Personal Logo"/>:  
-              <img src={expandMenuFilled} alt="Personal Logo"/> 
+              navlinks.map(link => {
+                return(
+                  <button key={link.id} onClick={link => navigateToSection(link)}>
+                    <h3 id={link.id} className="nav-links-text">{link.title}</h3>
+                  </button>
+                );
+              })   
             }
-          </button>
-          <button className="action-button">
+          </div>
+          <button className="night-mode-button">
             <img src={nightModeFilled} alt="Personal Logo"/>
           </button>
+          <button className="open-menu-button" onClick={()=>{openMenu(!menuOpened)}}>
+            <img src={expandMenuFilled} alt="Personal Logo"/>
+          </button>
+          </div>
         </div>
-      </div>
+        {
+          menuOpened && <ToggleableMenu opened={menuOpened} />
+        }
+      </>
     );
 }
 
